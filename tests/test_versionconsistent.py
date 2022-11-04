076e703c9233aed3e73f62a1e1750a94d8c8c88a
Wouter Ellenbroek <w.g.ellenbroek@tue.nl>
2022-11-04 02:35:28 +0100

    added the forgotten test

---
+++ tests/test_versionconsistent.py
@@ -0,0 +1,14 @@
+from magicpi import __version__
+import toml
+import os
+
+
+def test_version():
+    testdir = os.path.dirname(os.path.abspath(__file__))
+    tomlfile = os.path.join(testdir, "..", "pyproject.toml")
+    release = toml.load(tomlfile)['tool']['poetry']['version']
+    assert release == __version__
+
+
+if __name__ == "__main__":
+    test_version()
